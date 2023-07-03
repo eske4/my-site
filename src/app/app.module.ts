5e2203e7d1aa540ad0563035a02f39e5905b382a
eske4 <36681234+eske4@users.noreply.github.com>
2023-07-03 20:25:20 +0200

    Removed pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { Project1Component } from './project1/project1.component';
 import { Project2Component } from './project2/project2.component';
 import { Project3Component } from './project3/project3.component';
@@ -30,12 +29,6 @@ import { Project3Component } from './project3/project3.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
